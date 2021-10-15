de210fb044d94ffd87c7fa144cd8353e0838ccc9
heesun <gmltjs2030@gmail.com>
2021-10-15 17:24:18 +0900

    희선 첫번째 커밋

---
+++ src/main/java/com/koreait/yougn/YougnApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.yougn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YougnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YougnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/yougn/YougnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.yougn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YougnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
