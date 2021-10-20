6653fbd21830cce4e8b20b0fed0ceacd3d32c81b
ted <white19920@naver.com>
2021-10-20 23:43:48 +0900

    10-20 23:41 커밋

--- src/main/java/com/koreait/yougn/controller/BoardController.java
+++ src/main/java/com/koreait/yougn/controller/BoardController.java
@@ -10,18 +10,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board/*")
 public class BoardController {
 
-    @GetMapping("list")
-    public String notice(){
-        return "/board/list";
+    /*공지*/
+    @GetMapping("notice")     // 공지사항 리스트
+    public String notice(){return "/board/notice/notice";}
+
+    /*수매*/
+    @GetMapping("buyAndSell")   // 수매 리스트
+    public String buyAndSell(){
+        return "/board/buyAndSell/buyAndSell";
     }
 
-    @GetMapping("buyAndSale")
-    public String buyAndSale(){
-        return "/board/buyAndSale";
-    }
+    @GetMapping("buyRead")     // 삽니다 상세 보기
+    public String buyRead(){return "/board/buyAndSell/buyRead";}
 
-    @GetMapping("view")
-    public String view(){return "/board/view";}
-    @GetMapping("write")
-    public String write(){return "/board/write";}
+    @GetMapping("sellRead")     // 팝니다 상세 보기
+    public String sellRead(){return "/board/buyAndSell/sellRead";}
+
+    @GetMapping("buyWrite")    // 삽니다 글 작성
+    public String buyWrite(){return "/board/buyAndSell/buyWrite";}
+
+    @GetMapping("sellWrite")    // 삽니다 글 작성
+    public String sellWrite(){return "/board/buyAndSell/sellWrite";}
+
+    /*임대 매매*/
+    @GetMapping("rentalAndSale") // 임대 매매 리스트
+    public String rentalAndSale(){return "/board/rentalAndSale/rentalAndSale";}
+
+    @GetMapping("rentalRead")     // 임대 상세 보기
+    public String rentalRead(){return "/board/rentalAndSale/rentalRead";}
+
+    @GetMapping("saleRead")     // 매매 상세 보기
+    public String saleRead(){return "/board/rentalAndSale/saleRead";}
+
+    @GetMapping("rentalWrite")    // 임대 글 작성
+    public String rentalWrite(){return "/board/rentalAndSale/rentalWrite";}
+
+    @GetMapping("saleWrite")    // 매매 글 작성
+    public String saleWrite(){return "/board/rentalAndSale/saleWrite";}
+
+    /*고인물*/
+    @GetMapping("qnaList") // 임대 매매 리스트
+    public String qnaList(){return "/board/goinmool/qnaList";}
+
+    @GetMapping("qnaRead")     // 팝니다 상세 보기
+    public String qnaView(){return "/board/goinmool/qnaRead";}
+
+    @GetMapping("qnaRegister")    // 매매 글 작성
+    public String qnaRegister(){return "/board/goinmool/qnaRegister";}
+
 }
