bcad39f3cd87a9f1e70a285bf4a195888fb01ccb
ted <minjagot@naver.com>
2021-10-21 10:26:26 +0900

    민재 폰트 ++ 커밋

---
+++ src/main/java/com/koreait/yougn/controller/Supportcontroller.java
@@ -0,0 +1,29 @@
+package com.koreait.yougn.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/support/*")
+@RequiredArgsConstructor
+public class Supportcontroller {
+
+    @GetMapping("hallList")
+    public String hallList(){return "support/hallList";}
+
+    @GetMapping("returnList")
+    public String returnList(){return "support/returnList";}
+
+    @GetMapping("classList")
+    public String classList(){return "support/classList";}
+
+    @GetMapping("infoList")
+    public String infoList(){return "support/infoList";}
+
+    @GetMapping("classView")
+    public void classView(){}
+}
