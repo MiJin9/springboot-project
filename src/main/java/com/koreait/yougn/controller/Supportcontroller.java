05ba111c4388a1d0b0ea5412bed3b5511aab8fb4
ted <minjagot@naver.com>
2021-10-28 23:52:51 +0900

    민재 지역연락망 커밋 2021-10-28 23:52

--- src/main/java/com/koreait/yougn/controller/Supportcontroller.java
+++ src/main/java/com/koreait/yougn/controller/Supportcontroller.java
@@ -1,7 +1,9 @@
 package com.koreait.yougn.controller;
 
+import com.koreait.yougn.beans.vo.HallCri;
 import com.koreait.yougn.beans.vo.PageDTO;
 import com.koreait.yougn.beans.vo.ReturnCri;
+import com.koreait.yougn.services.HallService;
 import com.koreait.yougn.services.ReturnService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,9 +19,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Supportcontroller {
 
     private final ReturnService returnService;
+    private final HallService hallService;
 
     @GetMapping("hallList")
-    public String hallList(){return "support/hallList";}
+    public String hallList(HallCri hallCri, Model model){
+        if(hallCri.getKeyword() != null){
+            String temp =hallCri.getKeyword();
+            hallCri.setKeyword(temp.replace(" ","").length() == 0? null :hallCri.getKeyword());
+        }
+        if(hallCri.getSido() != null){
+            hallCri.setSido(hallCri.getSido().equals("시/도 선택")?null:hallCri.getSido());
+        }
+        if(hallCri.getSido() != null){
+            hallCri.setSido(hallCri.getSido().equals("")?null:hallCri.getSido());
+        }
+        if(hallCri.getGugun() != null) {
+            hallCri.setGugun(hallCri.getGugun().equals("전체")?null:hallCri.getGugun());
+        }
+        if(hallCri.getGugun() != null){
+            hallCri.setGugun(hallCri.getGugun().equals("")?null:hallCri.getGugun());
+        }
+        model.addAttribute("list",hallService.getList(hallCri));
+        model.addAttribute("pageMaker",new PageDTO(hallService.getTotal(hallCri),10,hallCri));
+        return "support/hallList";
+    }
 
     @GetMapping(value = "returnList")
     public String returnList(ReturnCri returnCri, Model model){
