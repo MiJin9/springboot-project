315ca97b27a864a3dbc27f31166f2e74abd04c2a
heesun <gmltjs2030@gmail.com>
2021-11-02 14:30:48 +0900

    희선 에러페이지 완성 커밋

--- src/main/java/com/koreait/yougn/controller/MainController.java
+++ src/main/java/com/koreait/yougn/controller/MainController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
